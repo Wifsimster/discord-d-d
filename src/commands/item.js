9e37c1707985960efdeac6ba627d69762b4e8f6e
Damien BATTISTELLA <damien.battistella@dedalus-group.com>
2020-10-21 17:33:52 +0200

    fix item command

--- src/commands/item.js
+++ src/commands/item.js
@@ -3,17 +3,16 @@ const { getItem } = require('../utils')
 module.exports = {
   name: 'item',
   async execute(message, args) {
-
     if(args[0]) {
       let item = await getItem(args[0])
 
       if(item) {
         message.channel.send(`\`${item.name}\`: ${item.description}`)
       } else {
-        message.channel.send('')
+        message.channel.send(`No item called \`${args[0]}\` found !`)
       }
     } else {
-      message.channel.send('')
+      message.channel.send(`**${message.author.username}** You have to specified an item name !`)
     }
   }
 }
