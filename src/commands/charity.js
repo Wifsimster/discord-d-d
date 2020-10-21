2e381fc9ac0d8fa3d0a232dec5e8c36291b8dbf1
Damien BATTISTELLA <damien.battistella@dedalus-group.com>
2020-10-21 17:59:21 +0200

    add charity command

---
+++ src/commands/charity.js
@@ -0,0 +1,19 @@
+const User = require('../models/user')
+const { throwDice, triggerEvent } = require('../utils')
+
+module.exports = {
+  name: 'charity',
+  async execute(message) {
+    let user = await User.findByPk(message.author.id)
+    let randomValue = throwDice()
+
+    message.channel.send(`**${user.username}** do charity on the sidewalk...`)
+
+    if(triggerEvent()) {
+      message.channel.send(`**${user.username}** someone give you ${randomValue} 🪙`)
+      await user.increment('coins', { by: randomValue })
+    } else {
+      message.channel.send(`**${user.username}** nobody gives a fuck about you !`)
+    }    
+  }
+}
