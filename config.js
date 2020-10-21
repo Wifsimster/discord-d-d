688d13d2fc69422168af350701718072addef6da
Damien BATTISTELLA <damien.battistella@dedalus-group.com>
2020-10-21 17:04:54 +0200

    update gitignore

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const prefix = 'beta'
-const token = 'NzY2NzE2NzA3MzYzNDg3NzU1.X4nakw.5FxDKawuyYXkZfkiwxRDW8Lqnrg'
+const token = 'NzY2NzE2NzA3MzYzNDg3NzU1.X4nakw.0eLbKkQxV4leiFn7OycSFU8XIWw'
 
 module.exports = { prefix, token }
